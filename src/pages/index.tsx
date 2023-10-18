c30f227bc7885c0973f1172790cab1fc31d68284
dsnsgithub <48170013+dsnsgithub@users.noreply.github.com>
2023-10-17 22:26:30 -0700

    update scheduli homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,14 @@ import Schedule from "../components/index/Schedule";
 import Status from "../components/index/Status";
 import Countdown from "../components/index/Countdown";
 
+import Image from "next/image";
+import homepagedark from "../../public/mobile/homepagedark.png";
+import changeperiodname from "../../public/mobile/changeperiodname.png";
+import homepagelight from "../../public/mobile/homepagelight.png";
+import importschedule from "../../public/mobile/importschedule.png";
+import notification from "../../public/mobile/notification.png";
+import scheduledark from "../../public/mobile/scheduledark.png";
+
 export interface ScheduleDB {
 	about: About;
 	routines: Routines;
@@ -196,22 +204,55 @@ export default function Home() {
 		);
 	}
 
+	// ## What is Scheduli
+	// Scheduli is a **general-purpose** schedule app avaliable on iOS and Android that keeps you informed about your daily schedule, even during the most chaotic days.
+
+	// ## Why Scheduli over competing apps?
+	// Scheduli offers a number of benefits against competing apps like Saturn.
+	// - Scheduli is a general-purpose schedule app, routines are **completely customizable** and can be used for home, work, and school.
+	// - Cross platform, avaliable on both Android and iOS
+	// - Notifications that display current event/progress to next event
+	// - Notifications that warn you if you don't have school tomorrow (planned feature)
+	// - School schedule presets are more updated/accurate
+	// - Doesn't require internet access to use core functionality
+
 	if (!scheduleDB) {
 		return (
 			<div className="container mx-auto mt-10 flex flex-col justify-center lg:p-8">
-				<div className="flex items-center justify-center flex-col shadow-xl rounded-lg p-10 lg:p-24 bg-wedgewood-300">
-					<h2 className="text-2xl mt-4 text-center">
-						It looks like you {"haven't"} set up a schedule yet. <br></br>
-						{/* You can either
-						<div className="flex items-center justify-around p-4">
-							<div>
-								<h2 className="text-xl font-bold">Create a New Schedule</h2>
-							</div>
+				<div className="flex items-center justify-center flex-col shadow-xl rounded-lg p-10 lg:p-24 bg-wedgewood-200 border-wedgewood-300 border-2">
+					<div className="flex flex-row items-center">
+						<div className="w-2/4 mr-10">
+							<h2 className="text-4xl mt-4 font-bold">What is Scheduli? </h2>
 
-							<div>
-								<h2 className="text-xl font-bold">Select from a Preset</h2>
-							</div>
-						</div> */}
+							<h2 className="text-2xl mt-4">
+								Scheduli is a <b>general-purpose</b> schedule app avaliable on iOS and Android that keeps you informed about your daily schedule, even during the most chaotic days.
+							</h2>
+
+							<h2 className="text-2xl mt-4">Avaliable on the App Store & Google Play Store soon.</h2>
+						</div>
+						<div className="flex flex-row items-center">
+							<Image src={homepagelight} alt="Scheduli Home page dark" height={600} className="mr-4"></Image>
+							<Image src={homepagedark} alt="Scheduli Home page dark" height={600}></Image>
+						</div>
+					</div>
+
+					<hr className="border-2 w-full mt-8 border-wedgewood-300"></hr>
+
+					<h2 className="text-4xl mt-10 font-bold">Why Scheduli over competing apps? </h2>
+
+					<h2 className="text-2xl mt-4 mb-4">Scheduli offers a number of benefits against competing apps.</h2>
+					<ul className="list-disc text-lg">
+						<li>Scheduli is a general-purpose schedule app, routines are completely customizable and can be used for home, work, and school.</li>
+						<li>
+							Cross platform, <b>avaliable on both Android and iOS</b>
+						</li>
+						<li>Notifications that display current event/progress to next event</li>
+						<li>School schedule presets are more updated/accurate</li>
+						<li>Does not require internet access to use core functionality</li>
+					</ul>
+
+					<h2 className="text-2xl text-center bg-wedgewood-300 border-wedgewood-400 border-2 p-4 rounded mt-16">
+						It looks like you {"haven't"} set up a schedule yet. <br></br>
 						Create a schedule{" "}
 						<Link className="text-blue-700 mt-4" href="/create">
 							here.
@@ -307,7 +348,7 @@ export default function Home() {
 		const tomorrowScheduleName = findCorrectSchedule(scheduleDB, new Date(currentDate.setDate(currentDate.getDate() + 1)));
 		if (tomorrowScheduleName != null) {
 			scheduleTimes = scheduleDB["routines"][tomorrowScheduleName]["events"];
-			
+
 			return (
 				<div className="container mx-auto mt-10 flex flex-col justify-center lg:p-8">
 					<Status time="" timeRange="" className="All events are over for today."></Status>
