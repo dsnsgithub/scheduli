83785057b7ea01b4d4bac44c49cbf5c4bb80efa5
dsnsgithub <48170013+dsnsgithub@users.noreply.github.com>
2023-10-27 20:04:40 -0700

    add android and ios links to redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+		{
+			source: "/android",
+			destination: "https://play.google.com/store/apps/details?id=com.scheduli.schedulimobile",
+			permanent: true
+		},
+		{
+			source: "/ios",
+			destination: "https://apps.apple.com/us/app/scheduli/id6470429917",
+			permanent: true
+		}
+	];
+  }
 }
 
 module.exports = nextConfig
